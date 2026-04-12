2736f6356dab73640e5e6514a7fa331f17cc29b2
baimasonga <baimasonga@gmail.com>
2026-04-12 11:43:01 +0000

    fix(analyze): add missing supabase_flutter import

--- mobile_app/lib/features/ai_feedback/ai_photo_screen.dart
+++ mobile_app/lib/features/ai_feedback/ai_photo_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 
