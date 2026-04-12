3b90de0f4858f40a371364dee96763667c0e241b
baimasonga <baimasonga@gmail.com>
2026-04-12 11:43:06 +0000

    fix(analyze): remove unused main.dart import

--- mobile_app/lib/features/notifications/notifications_screen.dart
+++ mobile_app/lib/features/notifications/notifications_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../../main.dart';
 
 class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({super.key});
